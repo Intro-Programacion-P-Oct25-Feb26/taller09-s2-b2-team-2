324e12dce4ae4a5442fd4d7523d8d5cfde5cd8b4
JOPA167 <joparajirj@gmail.com>
2025-12-11 17:16:24 -0500

    Solucion problema 04

--- EjerciciosT9/src/paquete02/Ejercicio04.java
+++ EjerciciosT9/src/paquete02/Ejercicio04.java
@@ -15,13 +15,37 @@ public class Ejercicio04 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         double[] promedios = {10, 10, 9.1, 7, 6.1, 4, 8};
-        String[] estudiantes = {"Kimberly Gonzalez", "Mark Hogan", 
+        String[] estudiantes = {"Kimberly Gonzalez", "Mark Hogan",
             "Teresa Martinez", "Julia Johnson", "Mark Cook", "Jennifer Manning",
             "Juan Vasquez"};
         String[] promediosCualitativos = new String[7];
-        
-    }
+        String cualitativo;
+        String cadena = "";
 
+        for (int i = 0; i < 7; i++) {
+            if (promedios[i] >= 0 && promedios[i] <= 5.9) {
+                cualitativo = "Regular";
+                promediosCualitativos[i] = cualitativo;
+            } else {
+                if (promedios[i] >= 6 && promedios[i] <= 8.9) {
+                    cualitativo = "Bueno";
+                    promediosCualitativos[i] = cualitativo;
+                } else {
+                    if (promedios[i] >= 9 && promedios[i] <= 10) {
+                        cualitativo = "Sobresaliente";
+                        promediosCualitativos[i] = cualitativo;
+                    }
+                }
+            }
+            cadena = String.format("%s%s promedio: %.2f Promedio cualitativo:%s\n",
+                    cadena,
+                    estudiantes[i],
+                    promedios[i],
+                    promediosCualitativos[i]);
+            
+        }
+        System.out.printf("%s",cadena);
+    }
 }
